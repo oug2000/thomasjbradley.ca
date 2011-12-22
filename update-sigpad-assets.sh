6cee3170d130733bf661db255286abbfe1b9aa63
Thomas J Bradley <theman@thomasjbradley.ca>
2011-12-22 17:08:44 -0500

    Added the demo page and reorganized CSS/files for easier implementation. Added Signature Pad asset downloader.

---
+++ update-sigpad-assets.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget -O static/jquery.signaturepad.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.min.js
+wget -O static/jquery.signaturepad.css https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.css
+wget -O static/flashcanvas.js https://raw.github.com/thomasjbradley/signature-pad/master/build/flashcanvas.js
+wget -O static/json2.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/json2.min.js
