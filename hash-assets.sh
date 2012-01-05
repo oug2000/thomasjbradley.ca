ad1d5aa43cf7752eaa5c52b12f17c91594a26b56
Thomas J Bradley <theman@thomasjbradley.ca>
2012-01-05 01:48:44 -0500

    Added static asset MD5 hashing and renaming thing.

---
+++ hash-assets.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# Finds all the static assets that include a build number
+# and replaces the build number with and MD5 hash
+# Searches and replaces all HTML files for the newly named static assets
+
+FILES=`find ./__remote/static -regex '.*/*.[0-9]\{10\}.min.*'`
+COMMANDS=''
+TEMPFILE="/tmp/$(basename $0).$$.tmp"
+
+for F in $FILES; do
+	HASH=`md5 -q ${F}`
+	FNAME=`echo ${F} | cut -d'/' -f 4`
+	NEWFNAME=`echo ${FNAME} | sed -e "s/[0-9]\{10\}/${HASH}/g"`
+	NEWF=`echo ${F} | sed -e "s/[0-9]\{10\}/${HASH}/g"`
+	COMMAND="s/${FNAME}/${NEWFNAME}/g"
+	COMMANDS="${COMMANDS}${COMMAND}\n"
+
+	mv $F $NEWF
+done
+
+echo -e $COMMANDS >> $TEMPFILE
+
+find ./__remote -name "*.html" -type f -print0 | xargs -0 sed -i '' -f $TEMPFILE
