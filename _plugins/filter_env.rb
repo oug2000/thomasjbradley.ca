ac2636013e221be6b5a47ab37c5cb52b5daa8742
Thomas J Bradley <theman@thomasjbradley.ca>
2012-01-04 19:32:04 -0500

    Moved env file reading to another module and method.

--- _plugins/filter_env.rb
+++ _plugins/filter_env.rb
@@ -1,16 +1,8 @@
 module Jekyll
+
   module EnvFilter
-    @@env = nil
-
     def env(input)
-      if @@env.nil?
-        envfile = File.expand_path(File.dirname(__FILE__) + '/../_env')
-        env = 'prod'
-
-        if File.exists?(envfile)
-          env = IO.read(envfile).strip!
-        end
-      end
+      env = Jekyll::EnvFilterHelper.get_env()
 
       if @context.registers[:config]
         url = @context.registers[:config]['env'][env][input]
@@ -19,6 +11,25 @@ module Jekyll
       end
 
       "#{url}"
+    end
+  end
+
+  # Using a separate module to hide this method from liquid
+  module EnvFilterHelper
+    @@env = nil
+
+    # Helper method to load the env file
+    def EnvFilterHelper.get_env(config=nil)
+      if @@env.nil?
+        envfile = File.expand_path(File.dirname(__FILE__) + '/../_env')
+        @@env = 'prod'
+
+        if File.exists?(envfile)
+          @@env = IO.read(envfile).strip!
+        end
+      end
+
+      @@env
     end
   end
 end
