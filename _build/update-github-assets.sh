a490955841d4a1e3d5a1dd34ad95a4876bb8aee6
Thomas J Bradley <theman@thomasjbradley.ca>
2012-02-26 15:36:52 -0500

    Fixed bug: Signature Pad exmaples weren't fully functional. Updated Signature Pad to most recent version.

--- _build/update-github-assets.sh
+++ _build/update-github-assets.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Update the assets used in the online examples
-wget -O static/jquery.signaturepad.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.min.js
-wget -O static/jquery.signaturepad.css https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.css
-wget -O static/flashcanvas.js https://raw.github.com/thomasjbradley/signature-pad/master/build/flashcanvas.js
-wget -O static/json2.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/json2.min.js
-wget -O static/sample-signature-output.js https://raw.github.com/thomasjbradley/signature-pad/master/examples/sample-signature-output.js
+wget -O static/signature-pad/jquery.signaturepad.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.min.js
+wget -O static/signature-pad/jquery.signaturepad.css https://raw.github.com/thomasjbradley/signature-pad/master/build/jquery.signaturepad.css
+wget -O static/signature-pad/flashcanvas.js https://raw.github.com/thomasjbradley/signature-pad/master/build/flashcanvas.js
+wget -O static/signature-pad/json2.min.js https://raw.github.com/thomasjbradley/signature-pad/master/build/json2.min.js
+wget -O static/signature-pad/sample-signature-output.js https://raw.github.com/thomasjbradley/signature-pad/master/examples/sample-signature-output.js
+wget -O static/signature-pad/flashcanvas.swf https://raw.github.com/thomasjbradley/signature-pad/master/build/flashcanvas.swf
+wget -O static/signature-pad/pen.cur https://raw.github.com/thomasjbradley/signature-pad/master/build/pen.cur
+wget -O static/signature-pad/journal.eot https://raw.github.com/thomasjbradley/signature-pad/master/build/journal.eot
+wget -O static/signature-pad/journal.svg https://raw.github.com/thomasjbradley/signature-pad/master/build/journal.svg
+wget -O static/signature-pad/journal.ttf https://raw.github.com/thomasjbradley/signature-pad/master/build/journal.ttf
+wget -O static/signature-pad/journal.woff https://raw.github.com/thomasjbradley/signature-pad/master/build/journal.woff
 
 # Update the version numbers
 wget -O _includes/sigpad/VERSION.txt https://raw.github.com/thomasjbradley/signature-pad/master/VERSION.txt
