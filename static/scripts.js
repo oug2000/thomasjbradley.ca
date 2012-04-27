679167304bc7b5bb7d4ef8ec82e1f3b407a85c60
Thomas J Bradley <theman@thomasjbradley.ca>
2012-04-27 17:55:01 -0400

    Made the navigation more usable on touch devices.

--- static/scripts.js
+++ static/scripts.js
@@ -297,4 +297,24 @@ if (document.getElementsByClassName) {
       }())
     }
   }())
+
+  if (Modernizr.touch) {
+    ;(function () {
+      var navBtn = document.getElementsByClassName('nav-expander')[0]
+        , navList = document.getElementsByClassName('nav-list')[0]
+        , navOpen = false
+
+      navBtn.addEventListener('click', function (ev) {
+        if (navOpen) {
+          removeClass(navBtn, 'is-nav-expanded')
+          removeClass(navList, 'is-nav-list-expanded')
+          navOpen = false
+        } else {
+          addClass(navBtn, 'is-nav-expanded')
+          addClass(navList, 'is-nav-list-expanded')
+          navOpen = true
+        }
+      })
+    })
+  }
 }
