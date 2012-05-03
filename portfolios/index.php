6916680a330df8e06d61fe3c1f2d305faabd03bd
Thomas J Bradley <theman@thomasjbradley.ca>
2012-05-03 15:47:40 -0400

    Added redirections for new folders.

---
+++ portfolios/index.php
@@ -0,0 +1,4 @@
+<?php
+
+header('Location: http://thomasjbradley.ca/portfolios/web/', true, 301);
+exit;
---
+++ portfolios/teaching/sample-assignments/index.php
@@ -0,0 +1,4 @@
+<?php
+
+header('Location: http://thomasjbradley.ca/portfolios/teaching/', true, 301);
+exit;
